9aefaa5c3a4d53ebb83f7fcf87bcc4ec94a89ea3
Michael Ishri <michael.ishri@gmail.com>
2025-01-01 12:51:05 +1300

    Adds shell class

---
+++ resources/cc/mishri/shell.groovy
@@ -0,0 +1,21 @@
+package cc.mishri
+
+@groovy.transform.Field
+class CommandParser implements Serializable {
+    private final def steps
+
+    CommandParser(steps) {
+        this.steps = steps
+    }
+
+    String executeAndParse(String command) {
+        // Use the sh step via the steps object
+        def output = steps.sh(
+            script: command,
+            returnStdout: true
+        ).trim()
+        
+        // Add your parsing logic here
+        return output
+    }
+}
