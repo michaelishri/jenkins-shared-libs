60702b7872f80e33083aba3e02163f5c85738e9a
Michael Ishri <michael.ishri@gmail.com>
2025-01-01 16:26:15 +1300

    WIP: LinuxDistro

--- src/traits/DateTime.groovy
+++ src/traits/DateTime.groovy
@@ -1,12 +1,8 @@
 package traits
 
-import com.cloudbees.groovy.cps.NonCPS
-
-trait DateTime implements Serializable {
+trait DateTimeTrait implements Serializable {
     @NonCPS
     String getDateTime() {
-        // Using a simpler date formatting approach
-        def now = new Date()
-        return now.format("yyyy-MM-dd HH:mm:ss", TimeZone.getTimeZone('UTC'))
+        return new Date().toString()
     }
 }
