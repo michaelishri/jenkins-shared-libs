1a295fffb63d24c822e98b72c94d69ff8bc1c0cd
Michael Ishri <michael.ishri@gmail.com>
2025-01-01 15:17:52 +1300

    WIP: CommandParser

--- src/machine/command/DiskUsage.groovy
+++ src/machine/command/DiskUsage.groovy
@@ -13,7 +13,15 @@ class DiskUsage implements Serializable {
     }
 
     String getHostname() {
-        return java.net.InetAddress.getLocalHost().getHostName()
+        if(hostname == null) {
+            hostname = java.net.InetAddress.getLocalHost().getHostName()
+        }
+
+        return hostname
+    }
+
+    def setHostname(String computername) {
+        hostname = computername
     }
 
     String getDateTime() {
