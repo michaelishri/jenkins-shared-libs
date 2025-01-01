085dce989262169cae181cffda20f92096386151
Michael Ishri <michael.ishri@gmail.com>
2025-01-01 16:50:40 +1300

    WIP: LinuxDistro

--- src/machine/command/TraitTesting.groovy
+++ src/machine/command/TraitTesting.groovy
@@ -1,11 +1,6 @@
 package machine.command
 
 class TraitTesting implements Serializable, LoggingTrait {
-
-    TraitTesting(steps) {
-        this.steps = steps
-    }
-
     String getName() {
         return "My name is...."
     }
