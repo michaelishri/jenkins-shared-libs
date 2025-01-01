fb3eeb1f962e191f38acf1a82f9dc383782c5e79
Michael Ishri <michael.ishri@gmail.com>
2025-01-01 13:25:58 +1300

    WIP: CommandParser

--- src/org/example/CommandParser.groovy
+++ src/org/example/CommandParser.groovy
@@ -1,34 +1,10 @@
 package org.example
 
-import com.cloudbees.groovy.cps.NonCPS
-import org.jenkinsci.plugins.workflow.cps.CpsScript
+class CommandParser {
+    def steps = Binding.getVariable("steps")
 
-class CommandParser implements Serializable {
-    // Remove the steps field and constructor
-    
-    def script = null
-    
-    // This method will be called automatically by Jenkins
-    def void setScript(CpsScript script) {
-        this.script = script
-    }
-
-    String executeAndParse(String command) {
-        // Use sh step directly via script object
-        def output = script.sh(
-            script: command,
-            returnStdout: true
-        ).trim()
-        
-        // Add your parsing logic here
-        return parseOutput(output)
-    }
-    
-    @NonCPS
-    private String parseOutput(String output) {
-        // Add parsing logic here
-        // @NonCPS annotation is needed for complex operations
-        // that aren't CPS-transformed
+    String parseCommandOutput(String command) {
+        def output = steps.sh(script: command, returnStdout: true).trim()
         return output
     }
 }
