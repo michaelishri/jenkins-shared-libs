25e80ca1dd3bd56aedd2d46daeaea7fbcb0390a8
Michael Ishri <michael.ishri@gmail.com>
2025-01-01 16:17:39 +1300

    WIP: LinuxDistro

--- src/traits/DateTime.groovy
+++ src/traits/DateTime.groovy
@@ -1,8 +1,10 @@
 package traits
 
 import java.text.SimpleDateFormat
+import com.cloudbees.groovy.cps.NonCPS
 
 trait DateTime implements Serializable {
+    @NonCPS
     String getDateTime() {
         def date = new Date()
         def dateTime = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
